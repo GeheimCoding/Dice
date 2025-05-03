4048fa98c500894eec8f735aad7e8bff2b17c9ff
GeheimCoding <geheim.coding@gmail.com>
2025-05-03 10:41:43 +0200

    Add avian3d

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
+use avian3d::prelude::*;
 use bevy::prelude::*;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins.set(WindowPlugin {
-            primary_window: Some(Window {
-                title: String::from("Dice"),
+        .add_plugins((
+            DefaultPlugins.set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: String::from("Dice"),
+                    ..default()
+                }),
                 ..default()
             }),
-            ..default()
-        }))
+            PhysicsPlugins::default(),
+        ))
         .run();
 }
