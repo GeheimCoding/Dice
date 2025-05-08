be7e8b1bfe46713caaed53e46da9ad1713f4067c
GeheimCoding <geheim.coding@gmail.com>
2025-05-08 19:20:19 +0200

    Add reference sphere

--- src/main.rs
+++ src/main.rs
@@ -13,5 +13,30 @@ fn main() {
             }),
             PhysicsPlugins::default(),
         ))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let sphere = Sphere::new(0.08);
+    commands.spawn((
+        Mesh3d(meshes.add(sphere)),
+        MeshMaterial3d(materials.add(Color::srgb(0.0, 0.8, 0.8))),
+    ));
+
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(0.0, 0.0, 0.5).looking_at(Vec3::ZERO, Vec3::Y),
+    ));
 }
